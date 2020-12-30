e296169f687e37aa9e363d2d8877541f6c954458
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-12-29 21:08:52 -0500

    Merge sort algorithm added

--- DataStructures-09-Sorting/src/MyInsertionSort.java
+++ DataStructures-09-Sorting/src/MyInsertionSort.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class MyInsertionSort {
+
+    public static int[] insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int current = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > current) {
+                // shifting operation
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = current;
+        }
+        return array;
+    }
 }
