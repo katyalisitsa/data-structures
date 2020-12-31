f9ac1144ed0a8854c1b951010168b48bedcceed8
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-12-30 19:42:33 -0500

    Search algorithm added

--- DataStructures-10-SearchArgorithms/src/SearchMainApp.java
+++ DataStructures-10-SearchArgorithms/src/SearchMainApp.java
@@ -1,12 +1,15 @@
 public class SearchMainApp {
     public static void main(String[] args) {
-        int[] myarray = new int[]{1, 2, 3, 4, 5, 6, 89, 100, 113, 109, 112};
+        int[] myarray = new int[]{1, 2, 3, 4, 5, 6, 89, 100, 113, 114, 120};
 
         //var mylinearsearch=new MyLinearSearch();
         //System.out.println("Linear Search : " +mylinearsearch.linearSearch(myarray,112));
 
-        var myBinarySearchRec = new MyBinarySearchRecursive();
-        System.out.println("Rec Bin Search: " + myBinarySearchRec.binarySearchRec(myarray, 112));
+        //var myBinarySearchRec = new MyBinarySearchRecursive();
+        //System.out.println("Rec Bin Search: " + myBinarySearchRec.binarySearchRec(myarray, 112));
+
+        var myBinarySearchIterative = new MyBinarySearchIterative();
+        System.out.println("Iterative Bin Search: " + myBinarySearchIterative.binSearchIterative(myarray, 120));
 
 
     }
